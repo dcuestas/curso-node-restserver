df2dfde51e14081d57fb4a91931e06ecf9072fb0
dcuestas <81628804+dcuestas@users.noreply.github.com>
2021-10-20 18:45:31 +0200

    Google Sign added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,10 @@
-const {response} = require('express');
+const {response, json} = require('express');
 const bcryptjs = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/generar-jws');
 const { JsonWebTokenError } = require('jsonwebtoken');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async(req, res = response) => {
@@ -46,11 +47,57 @@ const login = async(req, res = response) => {
         console.log(error);
         return res.status(500).json({
             msg: 'Hable con el administrador'
-        })
+        });
     }
-    
+}
+
+const googleSignIn = async(req, res = response) => {
+
+    const{id_token} = req.body;
+
+    try{
+        const {correo, nombre, img} = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({correo});
+
+        if (!usuario){
+            //Tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        // Si el usuario en DB
+        if(!usuario.estado){
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        // Generar el JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+        });
+
+    } catch (error){
+        return res.status(400).json({
+            msg: 'Token de Google no es válido'
+        });
+    }
+
 }
 
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const {check} = require('express-validator');
 
 const { validarCampos } = require('../middlewares/validar-campos');
 
-const { login } = require('../controllers/auth');
+const { login, googleSignIn } = require('../controllers/auth');
 
 const router = Router();
 
@@ -14,5 +14,10 @@ router.post('/login', [
     check('password', 'La contraseña es obligatoria').not().isEmpty(),
     validarCampos
 ], login);
+
+router.post('/google', [
+    check('id_token', 'id_token de google es necesario').not().isEmpty(),
+    validarCampos
+], googleSignIn);
 
 module.exports = router;
