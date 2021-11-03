bb385419d3b78cf37d815329808ed618521ad93d
dcuestas <81628804+dcuestas@users.noreply.github.com>
2021-11-03 19:04:57 +0100

    file upload added

--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 
 const {dbConnection} = require('../database/config');
 
@@ -11,10 +12,11 @@ class Server{
 
         this.paths = {
             auth:       '/api/auth',
-            buscar:       '/api/buscar',
+            buscar:     '/api/buscar',
             categorias: '/api/categorias',
-            productos: '/api/productos',
-            usuarios:   '/api/usuarios'
+            productos:  '/api/productos',
+            usuarios:   '/api/usuarios',
+            uploads:   '/api/uploads'
         }
 
         // Conectar a base de datos
@@ -41,6 +43,13 @@ class Server{
 
         // Directorio Público
         this.app.use(express.static('public'));
+
+        // Fileupload - Carga de archivos
+        this.app.use(fileUpload({
+            useTempFiles : true,
+            tempFileDir : '/tmp/',
+            createParentPath: true
+        }));
     }
 
     routes(){
@@ -49,6 +58,7 @@ class Server{
         this.app.use(this.paths.categorias, require('../routes/categorias'));
         this.app.use(this.paths.productos, require('../routes/productos'));
         this.app.use(this.paths.usuarios, require('../routes/usuarios'));
+        this.app.use(this.paths.uploads, require('../routes/uploads'));
     }
 
     listen(){
